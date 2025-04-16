a98979d4ac9cb990e14c4cac233e273fbbcae396
GulzinaB <gulzina1206@gmail.com>
2025-04-16 04:21:03 +0000

    new files

--- class6/main.tf
+++ class6/main.tf
@@ -1,8 +1,9 @@
 module "vpc-review" {
   source  = "GulzinaB/vpc-review/aws"
-  version = "0.1.0"
-  
+  version = "0.2.0"
+
   vpc_cidr = "172.31.0.0/16"
   subnet1_cidr ="172.31.1.0/24"
-  environment = "stage"
+  subnet2_cidr ="172.31.2.0/24"
+  environment = "qa"
 }
